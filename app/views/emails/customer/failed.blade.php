6f55da47a5430fd1791e033be0e9a4d8d9c8b37c
mavame <mvanmeter1@gmail.com>
2014-04-18 11:35:25 -0400

    Adds fialed payment email template.

---
+++ app/views/emails/customer/failed.blade.php
@@ -0,0 +1,8 @@
+@extends('emails.layout')
+@section('content')
+
+	<h1>Payment Failed</h1>
+
+	<p>We tried processing a payment for your subscription and it has failed. To update billing ... </p>
+
+@stop
