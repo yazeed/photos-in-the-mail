9f74bef74bc9cb276f03615553caa68b75a07dd1
mavame <mvanmeter1@gmail.com>
2014-04-29 12:59:44 -0400

    Updates cancel copy

--- app/views/users/cancel.blade.php
+++ app/views/users/cancel.blade.php
@@ -1,10 +1,10 @@
 @extends('layout')
 @section('content')
 <div class="holder cancel-confirm">
-	<h1>Are you sure you want to cancel?</h1>
-	<p>You can renew at any time. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 	incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+	<h1>Cancel Subscription</h1>
+	<p>We’re sorry to hear that you don’t wish to receive Photos in the Mail anymore. Please remember that you can downgrade your plan at anytime, but if you still want to cancel simply click the button below.</p>
 	<form action="/unsubscribe/authorize" class="cancel-plan">
-    	<input type="submit" class="normal" value="Cancel Plan">
+    	<input type="submit" class="normal" value="Submit">
 	</form>
 	<p><a class="back" href="/users/{{ $user->id }}">Return to my account</a></p>
 </div>
